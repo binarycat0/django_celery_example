a9963d564d4e5197beaa1e7b195a8d19ed83ede0
arakhmatulin <info@binarycat.ru>
2018-06-19 11:08:48 +0300

    index html

--- src/django_celery_example/urls.py
+++ src/django_celery_example/urls.py
@@ -13,12 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path
+from django.views.generic import TemplateView
 
 from main_application import views as my_app_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', my_app_views.Test.as_view())
+    path('api/', my_app_views.Test.as_view()),
+    url(r'^$', TemplateView.as_view(template_name='index.html'), name='home'),
 ]
